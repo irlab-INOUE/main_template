e2e4de6ab48cb1e94e6669b11474f4cb78b0d77a
KazumichiINOUE <k.inoue@oyama-ct.ac.jp>
2020-07-12 20:41:32 +0900

    first commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/***********************************
+ * main template
+ *
+ * Kazumichi INOUE <k.inoue@oyama-ct.ac.jp>
+ *
+ * Usage:
+ * 	mkdir build
+ * 	cd build
+ * 	cmake ..
+ * 	make
+ *  ./main
+ *
+ * Note:
+ * 	プログラミングの雛形としてお使いください
+ *
+ ************************************/
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
